147f015a0329add867f15acffd1204ba880e2c2a
Ruud Boon <ruud@t-boonproductions.nl>
2019-11-25 18:23:05 +0100

    Added smpte types

--- examples/mtc.js
+++ examples/mtc.js
@@ -9,6 +9,11 @@ if (inputs.length <= 0) {
 console.log('Connecting to: ' + inputs[0]);
 var input = new easymidi.Input(inputs[0]);
 
+// SMPTE Types
+// 0 = 24 fps
+// 1 = 25 fps
+// 2 = 30 fps (Drop-Frame)
+// 3 = 30 fps
 input.on('smpte', function (smpte) {
   console.log('smpte: ' + smpte.smpte + ' smpteType: ' + smpte.smpteType );
 });
