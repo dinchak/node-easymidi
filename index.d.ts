cdfa382278a1536941ceccb797e9db812a2a6eae
Stefan Schmelz <35258671+StefanSchmelz@users.noreply.github.com>
2020-09-14 13:54:46 +0200

    Add channel 0 to types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from "events";
 
-export type Channel = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15;
+export type Channel = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15;
 
 export interface Note {
   note: number;
